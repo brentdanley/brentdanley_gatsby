e8390c89ed1968e43081ab727d56550840e2a6b3
Brent Danley <brentdanley@Brents-iMac.local>
2020-08-28 13:59:52 -0400

    Styles header component.

--- src/components/header.js
+++ src/components/header.js
@@ -2,13 +2,10 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import styles from './header.module.scss'
+
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
+  <header className={styles.header}>
     <div
       style={{
         margin: `0 auto`,
@@ -16,7 +13,7 @@ const Header = ({ siteTitle }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <h1 style={{ margin: 0, fontFamily: `Fugaz One` }}>
         <Link
           to="/"
           style={{
