22eda7fd7e33cf5944816825476f9aa7465790df
Brent Danley <brentdanley@Brents-iMac.local>
2020-08-28 13:57:35 -0400

    Adds Google Fonts and Sass.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Brent Danley`,
+    description: `Science and relationships and laughs and fun. You've arrived at the place.`,
+    author: `@brentdanley`,
   },
   plugins: [
+    `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -27,6 +28,30 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Pacifico`,
+          },
+          {
+            family: `Fugaz One`,
+          },
+          {
+            family: `Luckiest Guy`,
+          },
+          {
+            family: `Satisfy`,
+          },
+          {
+            family: `Roboto`,
+            variants: [`400`, `700`],
+            subsets: [`latin`]
+          },
+        ],
+      },
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
